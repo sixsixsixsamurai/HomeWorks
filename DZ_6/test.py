5b0316eb0f858547ab5490a7e7d8fb92470cce95
Mykola <120165143+sixsixsixsamurai@users.noreply.github.com>
2022-12-21 13:59:32 +0200

    Delete test.py

--- DZ_6/test.py
+++
@@ -1,8 +0,0 @@
-password = input("Введите ваш пароль : ")
-for char in password:
-    if char == "!" or char == "@" or char == "#":
-        print("+")
-        break
-else:
-    print("net !@#$%^&*(")
-
