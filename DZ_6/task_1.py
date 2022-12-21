7bb101a3f30655190546ca0d27d684f3b404337f
Mykhola <kradu2001@gmail.com>
2022-12-21 13:56:52 +0200

    gg

--- DZ_6/task_1.py
+++ DZ_6/task_1.py
@@ -1,32 +1,41 @@
 password = input("Введите ваш пароль : ")
 password_score = 0
-feedback = " "
-#проверка на длину 8+ строк
+feedback = ""
+
 if len(password) >= 8:
     password_score += 1
 else:
     feedback += "\nВаш пароль слишком короткий (используйте 8+ символов)"
-    #проверка на хоть одину цифру
+
 for char in password:
     if char.isdigit():
         password_score += 1
         break
 else:
-        feedback += "\nДобавьте цифры в ваш пароль"
-# проверка на верхний регистр
+        feedback += "\nДобавьте хотя бы одну цифру в ваш пароль"
+
 for char in password:
     if char.isupper():
         password_score += 1
         break
 else:
-    feedback += "\nТребуется хотя бы одна большая буква"
-    #провверка на нижний регистр
+    feedback += "\nДобавьте хотя бы одну большую букву"
+
 for char in password:
     if char.islower():
         password_score += 1
         break
 else:
-    feedback += "\nТребуется хотя бы одна маленькая буква"
+    feedback += "\nДобавьте хотя бы одну маленькую букву"
 
+for char in password:
+    if char.isalnum() == False:
+        password_score += 1
+        break
+else:
+    feedback +="\nДобавьте хотя бы один спец.символ"
 
-print(f"Ваша оценка за пароль {password_score} , вот фид бек : {feedback}")
+if password_score < 5:
+    print(f"Ваша оценка за пароль {password_score} баллов \nВот наши рекомендации для улучшения вашего пароля {feedback}")
+else:
+    print(f"Ваша оценка за пароль {password_score} баллов , ваш пароль превосходен")
